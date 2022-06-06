57ac41418e72e6e6cbf1a9bc5188aaaf7c385b45
miirsery <miirsery.main@gmail.com>
2022-06-06 11:04:21 +0300

    changed fractiosn

---
+++ src/components/Slider.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="slider">
+    <div class="slider__content container">
+      <swiper
+          :slidesPerView="1"
+          :loopFillGroupWithBlank="true"
+          :pagination="{
+            el: '.main-slider__fractions',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+              return `<span>${current}</span>` + '|' + `<span>${(total - 1)}</span>`
+             }
+            }"
+          :navigation="{
+          nextEl: '.main-slider__arrow-next',
+          prevEl: '.main-slider__arrow-prev'
+        }"
+          class="main-slider swiper"
+      >
+        <swiper-slide class="main-slider__slide">
+          <div class="main-slider__slide-text">Название бога</div>
+          <div class="main-slider__slide-video">
+          <span>
+            Video
+          </span>
+            <img src="../assets/images/slide-1.jpg" alt="mugik">
+          </div>
+        </swiper-slide>
+        <swiper-slide class="main-slider__slide">
+          <div class="main-slider__slide-text">Название бога</div>
+          <div class="main-slider__slide-video">
+          <span>
+            Video
+          </span>
+            <img src="../assets/images/slide-1.jpg" alt="mugik">
+          </div>
+        </swiper-slide>
+        <swiper-slide class="main-slider__slide">
+          <div class="main-slider__slide-text">Назваqwdqwdqwdqfние бога</div>
+          <div class="main-slider__slide-video">
+          <span>
+            Video
+          </span>
+            <img src="../assets/images/slide-1.jpg" alt="mugik">
+          </div>
+        </swiper-slide>
+        <swiper-slide class="main-slider__slide">
+          <div class="main-slider__slide-text">123 бога</div>
+          <div class="main-slider__slide-video">
+          <span>
+            Video
+          </span>
+            <img src="../assets/images/slide-1.jpg" alt="mugik">
+          </div>
+        </swiper-slide>
+      </swiper>
+      <div class="main-slider__arrows">
+        <div class="main-slider__arrow-next"><span>Далее</span></div>
+        <div class="main-slider__arrow-prev"><span>Назад</span></div>
+      </div>
+      <div class="main-slider__fractions"></div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
+</script>
+<style lang="scss">
+.slider {
+  height: 100%;
+
+  &__content {
+    position: relative;
+    height: 100%;
+  }
+
+  .swiper-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding-bottom: 60px;
+  }
+}
+.main-slider {
+  &__slide {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+
+    &-text {
+      font-size: 40px;
+      line-height: 130%;
+      letter-spacing: 0.05em;
+      color: #FFFFFF;
+      margin-bottom: 40px;
+    }
+
+    &-video {
+      max-width: 954px;
+      max-height: 650px;
+
+      span {
+        display: inline-flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 32px;
+        object-fit: cover;
+      }
+    }
+  }
+
+  &__arrow-prev,
+  &__arrow-next {
+    cursor: pointer;
+    position: relative;
+    z-index: 999;
+    span {
+      font-weight: 400;
+      display: inline-block;
+      position: relative;
+      font-size: 20px;
+      line-height: 130%;
+      letter-spacing: 0.05em;
+      color: #FEFEFE;
+    }
+  }
+
+  &__arrow-prev{
+    right: 15%;
+    position: absolute;
+    bottom: 5%;
+
+    span {
+      left: 35px;
+      top: -6px;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 25px;
+      height: 16px;
+      left: 0;
+      pointer-events: none;
+      background: center/ cover no-repeat url('../assets/images/arrow.svg');
+      transform: rotate(180deg);
+    }
+  }
+
+  &__arrow-next {
+    position: absolute;
+    bottom: 5%;
+    right: 0;
+
+    span {
+      left: -37px;
+      top: -6px;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 25px;
+      height: 16px;
+      right: 0;
+      pointer-events: none;
+      background: center/ cover no-repeat url('../assets/images/arrow.svg');
+    }
+  }
+
+  &__fractions {
+    position: absolute;
+    display: inline-flex;
+    justify-content: center;
+    left: 50%;
+    top: 15px;
+    font-size: 24px;
+    line-height: 130%;
+    letter-spacing: 0.05em;
+    color: #FEFEFE;
+    transform: translateX(-50%);
+
+    span {
+      display: inline-block;
+
+      &:first-child {
+        margin-right: 5px;
+      }
+
+      &:last-child {
+        margin-left: 5px;
+      }
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,116 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__wrapper container">
+      <div class="home__content" v-if="!isSlider">
+        <div class="home__left">
+          <div class="home__title">12 богов <br> Скандинавской мифологии</div>
+          <p class="home__text">Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно Описание об блоге или о чем угодно </p>
+          <button class="home__button" @click="handleStep"><span>На следующий шаг</span></button>
+        </div>
+        <div class="home__right">
+          <img src="../assets/images/horse.jpg" alt="horse">
+        </div>
+      </div>
+      <slider v-else />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import {ref} from "vue";
+import Slider from '@/components/Slider'
+const isSlider = ref(false)
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+const handleStep = () => {
+  isSlider.value = true
 }
 </script>
+
+<style lang="scss">
+.home {
+  background: center/ cover no-repeat url('../assets/images/first-slide.jpg');
+  height: 100vh;
+
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+  }
+
+  &__content {
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+  }
+
+  &__left {
+    max-width: 710px;
+  }
+
+  &__title {
+    margin-bottom: 35px;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 130%;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    color: #FEFEFE;
+  }
+
+  &__text {
+    font-size: 24px;
+    line-height: 130%;
+    letter-spacing: 0.05em;
+    color: #FEFEFE;
+    margin-bottom: 55px;
+  }
+
+  &__button {
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 130%;
+    letter-spacing: 0.05em;
+    color: #FEFEFE;
+    position: relative;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    width: 467px;
+    height: 100px;
+    justify-content: space-around;
+    align-items: center;
+    align-content: center;
+
+    span {
+      display: inline-block;
+      left: -10px;
+      position: relative;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      width: 96%;
+      height: 99%;
+      bottom: 0;
+      background: center/ cover no-repeat url('../assets/images/frame.svg');
+    }
+  }
+
+  &__right {
+    max-width: 590px;
+    max-height: 650px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
