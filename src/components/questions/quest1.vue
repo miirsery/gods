a2bdcb2ec65154c6aa2e3b321534c79877a12e4e
miirsery <miirsery.main@gmail.com>
2022-06-08 23:00:57 +0300

    added questions

---
+++ src/components/questions/quest1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="question">
+    <h1 class="question__title">Вопрос2 тут прям тут</h1>
+    <div class="question__items">
+      <div class="question__item">
+        <div class="question__number">1</div>
+        <p class="question__ask">Ответ</p>
+      </div>
+      <div class="question__item">
+        <div class="question__number">2</div>
+        <p class="question__ask">Ответ</p>
+      </div>
+      <div class="question__item">
+        <div class="question__number">3</div>
+        <p class="question__ask">Ответ</p>
+      </div>
+      <div class="question__item">
+        <div class="question__number">4</div>
+        <p class="question__ask">Ответ</p>
+      </div>
+    </div>
+  </div>
+</template>
+
--- src/views/questions.vue
+++ src/views/questions.vue
@@ -1,22 +1,140 @@
 <template>
   <div class="questions">
-    <h1 class="questions__title">Вопрос тут прям тут</h1>
-    <div class="questions__items">
-      <div class="questions__item">
-        ответ
-      </div>
-    </div>
+    <keep-alive>
+      <component :is="currentSlideData" />
+    </keep-alive>
+    <button class="questions__button" @click="handleToggleSlide">Продолжить</button>
   </div>
 </template>
+<script>
+import {defineAsyncComponent, defineComponent, ref} from "vue";
+
+export default defineComponent({
+  components: {
+    quest0: defineAsyncComponent(() => import(`@/components/questions/quest0.vue`)),
+    quest1: defineAsyncComponent(() => import(`@/components/questions/quest1.vue`)),
+    quest2: defineAsyncComponent(() => import(`@/components/questions/quest2.vue`))
+  },
+
+  setup() {
+    const questions = [
+      {
+        id: 1,
+        questionTitle: 'quest',
+        title: 'Вопрос1 тут прям тут',
+        asks: [
+          {
+            id: 1,
+            value: 'Какой-то ответ 1',
+            correct: true,
+          },
+          {
+            id: 2,
+            value: 'Какой-то ответ 2',
+            correct: false,
+          },
+          {
+            id: 3,
+            value: 'Какой-то ответ 3',
+            correct: false,
+          },
+          {
+            id: 4,
+            value: 'Какой-то ответ 4',
+            correct: false,
+          }
+        ]
+      },
+      {
+        id: 2,
+        questionTitle: 'quest1',
+        title: 'Вопрос2 тут прям тут',
+        asks: [
+          {
+            id: 1,
+            value: 'Какой-то ответ 1',
+            correct: true,
+          },
+          {
+            id: 2,
+            value: 'Какой-то ответ 2',
+            correct: false,
+          },
+          {
+            id: 3,
+            value: 'Какой-то ответ 3',
+            correct: false,
+          },
+          {
+            id: 4,
+            value: 'Какой-то ответ 4',
+            correct: false,
+          }
+        ]
+      },
+      {
+        id: 3,
+        questionTitle: 'quest2',
+        title: 'Вопрос3 тут прям тут',
+        asks: [
+          {
+            id: 1,
+            value: 'Какой-то ответ 1',
+            correct: true,
+          },
+          {
+            id: 2,
+            value: 'Какой-то ответ 2',
+            correct: false,
+          },
+          {
+            id: 3,
+            value: 'Какой-то ответ 3',
+            correct: false,
+          },
+          {
+            id: 4,
+            value: 'Какой-то ответ 4',
+            correct: false,
+          }
+        ]
+      }
+    ]
+
+    const currenSlide = ref(0)
+    const currentSlideData = ref('quest0')
+
+    const handleToggleSlide = () => {
+      currenSlide.value++
+
+      currentSlideData.value = questions[currenSlide.value].questionTitle
+    }
+    return {
+      questions,
+      currentSlideData,
+      handleToggleSlide
+    }
+  }
+})
+
+</script>
 <style scoped lang="scss">
 .questions {
-  &__title {
-    font-weight: 500;
-    font-size: 48px;
+  height: 100%;
+  &__button {
+    position: absolute;
+    bottom: 10%;
+    left: 50%;
+    width: 450px;
+    height: 85px;
+    border-radius: 24px;
+    font-size: 24px;
     line-height: 130%;
     letter-spacing: 0.05em;
     color: #FEFEFE;
     text-align: center;
+    background: #BB6BD9;
+    transform: translateX(-50%);
   }
 }
 </style>
